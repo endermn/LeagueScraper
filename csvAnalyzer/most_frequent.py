b20606d75fa7f5ce6448a2b45abfc7868434ebaf
gevla <viktorboyadjiev@gmail.com>
2023-08-30 19:44:42 +0200

    qol changes

--- csvAnalyzer/most_frequent.py
+++ csvAnalyzer/most_frequent.py
@@ -3,16 +3,17 @@ from typing import List
 
 class Most_Frequent_Values:
     def __init__(self) -> None:
-        self.first_line = self.read_first_line()
+        self.data_file = '../player_data.csv'
+        self.first_line = self.__read_first_line()
         
-    def read_first_line(self) -> List[str]:
-        with open('../data.csv', 'r', encoding='utf-8') as file:
+    def __read_first_line(self) -> List[str]:
+        with open(self.data_file, 'r', encoding='utf-8') as file:
             return file.readline().split(',')
     def write_data(self) -> None:        
-        df = pd.read_csv('../data.csv')
-        with open('frequent.csv', 'w', encoding='utf-8') as file:
+        df = pd.read_csv(self.data_file)
+        with open('../average_stats.csv', 'w', encoding='utf-8') as file:
             for i in range(2, len(self.first_line) - 2):
-                file.write(f"{self.first_line[i]}: " + df[self.first_line[i]].value_counts().idxmax() + "\n")
+                file.write(f"{self.first_line[i]}:, " + df[self.first_line[i]].value_counts().idxmax() + "\n")
 
 if __name__ == "__main__":
     Most_Frequent_Values().write_data()
