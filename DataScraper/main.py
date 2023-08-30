79b37b579e3f1a1e26917581dd81017ee09bdbce
gevla <viktorboyadjiev@gmail.com>
2023-08-30 19:42:04 +0200

    qol changes

--- DataScraper/main.py
+++ DataScraper/main.py
@@ -6,27 +6,28 @@ import ProfileScraper
 import FileManager
 
 HEADERS: dict[str, str] = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
-PAGES: int = 10
+PAGES: int = 2
+
 
 if __name__ == "__main__":
-    FileManager.Manager().clear_file('../data.csv')
+    DATA_FILE = "../player_data.csv"
+    
+    FileManager.Manager().clear_file(DATA_FILE)
+    with open(DATA_FILE, 'a', encoding="utf-8") as file:
+        file.write('Rank, IGN, Tier, LP, Win-Lose, Winrate, Most Played, KDA, Avg CS, KP, \n')
 
-    file = open('../data.csv', 'a', encoding="utf-8")
-    file.write('Rank, IGN, Tier, LP, Win-Lose, Winrate, Most Played, KDA, Avg CS, KP, \n')
+        for i in range(PAGES + 1):
+            url = f"https://www.op.gg/leaderboards/tier?region=euw&page={i}"
 
-    for i in range(PAGES + 1):
-        url = f"https://www.op.gg/leaderboards/tier?region=euw&page={i}"
+            try:
+                resp = requests.get(url, headers=HEADERS, timeout=60)
+            except requests.exceptions.ConnectionError as e:
+                pass
+            soup = BeautifulSoup(resp.content, "lxml")
 
-        try:
-            resp = requests.get(url, headers=HEADERS, timeout=60)
-        except requests.exceptions.ConnectionError as e:
-            pass
-        soup = BeautifulSoup(resp.content, "lxml")
-
-        for i in ProfileScraper.Scraper(soup).scrape_profiles():
-            Analyzer = Profile_Analyzer('https://op.gg' + i)
-            file.write(Analyzer.get_data())
-    file.close()
+            for i in ProfileScraper.Scraper(soup).scrape_profiles():
+                Analyzer = Profile_Analyzer('https://op.gg' + i)
+                file.write(Analyzer.get_data())
 # TODO:
 #   Implement for all regions
 #   Search on conditions(wr, champ, rank, )
